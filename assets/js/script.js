96358b4879f82bc2b2edda87eecc8f427c22aa92
Roma <stop.ryadnov44@gmail.com>
2024-06-07 11:10:33 +0300

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,3 @@
-document
-  .querySelectorAll("#footer-custom-style .col")
-  .forEach((col) => (col.style.marginBottom = "0"));
-document
-  .querySelectorAll("#footer-custom-style .nav-item")
-  .forEach((item) => (item.style.marginBottom = "0"));
-document.querySelectorAll("#footer-custom-style .nav-link").forEach((link) => {
-  link.style.paddingTop = "0";
-  link.style.paddingBottom = "0";
-});
 /*Стрелка */
 window.onscroll = function () {
   scrollFunction();
